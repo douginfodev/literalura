0307ccd45ac5c21ae4e725c7df9b8a60a592660f
= <=>
2024-05-16 20:33:41 -0300

    feat: void printScreen created

--- src/main/java/com/douginfodev/literalura/view/UserInterface.java
+++ src/main/java/com/douginfodev/literalura/view/UserInterface.java
@@ -1,22 +1,30 @@
 package com.douginfodev.literalura.view;
 
+import java.util.Scanner;
+
 public class UserInterface {
-  
-    public UserInterface(){
-        System.out.println("BEM VINDO AO LITERALURA"); 
+    Scanner scanner = new Scanner(System.in);
+    int menuNumber = 0;
+
+    public UserInterface() {
+        System.out.println("BEM VINDO AO LITERALURA");
     }
 
-    public void printMenu(){
-        System.out.println("===========================");
-        System.out.println("    LITERALURA 1.0    ");
-        System.out.println("===========================");
-        System.out.println("         MENU              ");
-        System.out.println("1 = BUSCAR LIVROS");
-        System.out.println("2 = VISUALIZAR LIVROS");
-        System.out.println("3 = CADASTRAR");
-        System.out.println("4 = DOLAR -> EURO");
-        System.out.println("5 = EURO  -> REAL");
-        System.out.println("6 = EURO  -> DOLAR");
-        System.out.println("SELECIONE UMA OPÇÃO");
+    public void printMenu() {
+
+        while (menuNumber != 6) {
+            System.out.println("===========================");
+            System.out.println("    LITERALURA 1.0    ");
+            System.out.println("===========================");
+            System.out.println("         MENU              ");
+            System.out.println("1 = BUSCAR LIVROS / TITULO");
+            System.out.println("2 = LISTAR LIVROS REGISTRADO");
+            System.out.println("3 = LISTAR AUTORES REGISTRADOS ");
+            System.out.println("4 = LISTAR AUTORES / ANO");
+            System.out.println("5 = LISTAR LIVROS / IDIOMA");
+            System.out.println("6 = SAIR");
+            System.out.println("SELECIONE UMA OPÇÃO");
+            menuNumber = scanner.nextInt();
+        }
     }
 }
