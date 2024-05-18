f3f5444aa5beed0318df9ed806cdeb53fd5ccd1c
= <=>
2024-05-18 20:20:12 -0300

    feat: AutorRepository.java created

---
+++ src/main/java/com/douginfodev/literalura/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.douginfodev.literalura.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douginfodev.literalura.model.Autor;
+
+public interface AutorRepository extends JpaRepository<Autor,Integer> {
+
+}
