2ad33b81c86094242e46b38cb8fc41d70df84ed3
= <=>
2024-05-18 19:37:37 -0300

    feat: Model Autor.java created

---
+++ src/main/java/com/douginfodev/literalura/model/Autor.java
@@ -0,0 +1,41 @@
+package com.douginfodev.literalura.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Autor(){}
+
+    public Autor(String nome) {
+        this.nome = nome;
+    }
+
+    
+}
