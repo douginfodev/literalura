9ac13fb8a2b121f1632ff2a33abd733495f2699d
= <=>
2024-05-17 23:52:37 -0300

    feat: LiteraturaApplication.java updated. Add repository property

--- src/main/java/com/douginfodev/literalura/LiteraluraApplication.java
+++ src/main/java/com/douginfodev/literalura/LiteraluraApplication.java
@@ -1,13 +1,18 @@
-package com.douginfodev.literalura.literalura;
+package com.douginfodev.literalura;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.douginfodev.literalura.repository.LivroRepository;
 import com.douginfodev.literalura.view.UserInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
+    @Autowired
+    private LivroRepository repository; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
@@ -15,7 +20,7 @@ public class LiteraluraApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		UserInterface screenMenu = new UserInterface();
+		UserInterface screenMenu = new UserInterface(repository);
 	    screenMenu.printMenu();
 	}
 
