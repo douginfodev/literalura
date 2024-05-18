359b08385ccdfcf762239f1c0bb8b15a0933641d
= <=>
2024-05-17 23:49:11 -0300

    feat: UserInterface Updated

--- src/main/java/com/douginfodev/literalura/view/UserInterface.java
+++ src/main/java/com/douginfodev/literalura/view/UserInterface.java
@@ -1,13 +1,19 @@
 package com.douginfodev.literalura.view;
 
+import java.util.List;
 import java.util.Scanner;
+
+import com.douginfodev.literalura.model.Livro;
+import com.douginfodev.literalura.repository.LivroRepository;
 
 public class UserInterface {
     Scanner scanner = new Scanner(System.in);
     int menuNumber = 0;
+ 
+    private LivroRepository repository;
 
-    public UserInterface() {
-        System.out.println("BEM VINDO AO LITERALURA");
+    public UserInterface(LivroRepository repository) {
+        this.repository = repository;
     }
 
     public void printMenu() {
@@ -25,6 +31,40 @@ public class UserInterface {
             System.out.println("6 = SAIR");
             System.out.println("SELECIONE UMA OPÇÃO");
             menuNumber = scanner.nextInt();
+
+            if (menuNumber == 1){
+              InsertLivro();
+              System.out.println("ENTREI NO :" + menuNumber);
+            }
+
+            if (menuNumber == 2){
+                SelectLivro();
+                System.out.println("ENTREI NO SELECT :" + menuNumber);
+              }
         }
     }
+
+    private void InsertLivro(){
+        Livro livro = new Livro("Livro Teste","Autor Teste","PT-BR",1234,"2024");  
+        
+        try {
+            repository.save(livro);
+        } catch (Exception e) {
+            System.out.println("ERROOO :" + e.getMessage());
+        }  
+        
+    }
+
+    private void SelectLivro(){
+        
+        try {
+            List<Livro> livros = repository.findAll(); 
+            System.out.println(livros.toString());
+        } catch (Exception e) {
+            System.out.println("ERROOO :" + e.getMessage());
+        }  
+        
+    }
+
+
 }
